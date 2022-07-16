2b89a2376c13b3d1df8b5f94dc6b532e66d36a26
Abhijeet Kumar <67384659+Abjtkr05@users.noreply.github.com>
2022-07-16 19:05:06 +0530

    kotlin.main

--- firstKotlinProject/app/src/main/java/com/example/firstkotlinproject/MainActivity.kt
+++ firstKotlinProject/app/src/main/java/com/example/firstkotlinproject/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.firstkotlinproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        add.setOnClickListener(View.OnClickListener() {
+            val result = num1.text.toString().toInt() + num2.text.toString().toInt()
+            ans.text = result.toString();
+        })
     }
 }
