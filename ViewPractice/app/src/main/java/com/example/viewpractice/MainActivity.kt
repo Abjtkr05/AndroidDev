9ce87b6221d4d63540c0839736a914217a775d72
Abhijeet Kumar <67384659+Abjtkr05@users.noreply.github.com>
2022-07-16 20:11:41 +0530

    ViewPractice

--- ViewPractice/app/src/main/java/com/example/viewpractice/MainActivity.kt
+++ ViewPractice/app/src/main/java/com/example/viewpractice/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.viewpractice
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
@@ -8,8 +9,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Linear.setOnClickListener({
-            intent
-        })
+        Linear.setOnClickListener{
+            intent = Intent(this,LinearLayout::class.java)
+            startActivity(intent)
+        }
     }
 }
