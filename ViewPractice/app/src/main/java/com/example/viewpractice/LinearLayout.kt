198cb4fbb3566c44837ac0233e248579e4db1306
Abhijeet Kumar <67384659+Abjtkr05@users.noreply.github.com>
2022-07-16 20:03:42 +0530

    ViewPractice

--- ViewPractice/app/src/main/java/com/example/viewpractice/LinearLayout.kt
+++ ViewPractice/app/src/main/java/com/example/viewpractice/LinearLayout.kt
@@ -2,14 +2,10 @@ package com.example.viewpractice
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class LinearLayout : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        Linear.setOnClickListener({
-            intent
-        })
+        setContentView(R.layout.activity_linear_layout)
     }
 }
